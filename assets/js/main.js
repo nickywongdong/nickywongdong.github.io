44e4533d28b6a31bc2a6df3c24f3db09ab7e8b69
wongnich <wongnich@gmail.com>
2017-11-01 12:01:45 -0700

    debugging sticky

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,18 +4,6 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-/*var win = $(window),
-    nav = $('nav'),
-
-    pos = nav.offset().top,
-    sticky = function(){ 
-      win.scrollTop() > pos ?
-        nav.addClass('sticky')
-      : nav.removeClass('sticky')
-    }
-
-win.scroll(sticky)*/
-
 
 (function($) {
 
