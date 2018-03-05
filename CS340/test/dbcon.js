b5a04223fb496d4b3819b67ecdcabea706a216e0
wongnich <wongnich@gmail.com>
2018-03-04 16:24:58 -0800

    updating CS340 directory

--- CS340/test/dbcon.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-var pool = mysql.createPool({
-  connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_wolfordj',
-  password        : '7235',
-  database        : 'cs340_wolfordj'
-});
-module.exports.pool = pool;
--- CS340/test/public/deleteperson.js
+++
@@ -1,9 +0,0 @@
-function deletePerson(id){
-    $.ajax({
-        url: '/people/' + id,
-        type: 'DELETE',
-        success: function(result){
-            window.location.reload(true);
-        }
-    })
-};
